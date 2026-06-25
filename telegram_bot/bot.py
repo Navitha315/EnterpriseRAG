c53d38cb0bd0f96b90647c20c96a61fbda21e136
NAVITHA Elango <168547486+Navitha315@users.noreply.github.com>
2026-06-26 00:00:15 +0530

    Add files via upload

---
+++ telegram_bot/bot.py
@@ -0,0 +1,73 @@
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+import os
+import requests
+
+TOKEN = "8988662987:AAH3uHda8byWXB36s8qLgCwvjzCK5HSi0Qo"
+
+API_URL = "http://127.0.0.1:8000/query"
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    await update.message.reply_text(
+        "👋 Hello!\n\nAsk me anything about your enterprise documents."
+    )
+
+
+async def ask(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    question = update.message.text
+
+    try:
+
+        response = requests.post(
+            API_URL,
+            json={
+                "question": question
+            }
+        )
+
+        data = response.json()
+    
+        answer = data["answer"]
+
+        sources = "\n".join(
+            [
+                f"• {os.path.basename(doc['source'])}"
+                for doc in data["documents"]
+            ]
+        )
+
+        msg = (
+            f"🤖 {answer}\n\n"
+            f"📄 Sources:\n{sources}"
+        )
+
+        await update.message.reply_text(msg)
+
+    except Exception as e:
+
+        await update.message.reply_text(str(e))
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.add_handler(
+    MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        ask
+    )
+)
+
+print("Bot Running...")
+
+app.run_polling()
